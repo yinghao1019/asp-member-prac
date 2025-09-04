6ac1bf4d798f1364dd0e9b095a1a52fe4e0d187e
Howard.YH.Hung <howard.yh.hung@fii-foxconn.com>
2025-09-04 17:45:11 +0800

    feat: 取得員工資訊顯示畫面

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using asp_member_prac.Models;
+using asp_member_prac.Data;
 
 namespace asp_member_prac.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly EmployeeDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, EmployeeDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
-        return View();
+
+        var employees = _context.Employees.ToList();
+        return View(employees);
     }
 
     public IActionResult Privacy()
